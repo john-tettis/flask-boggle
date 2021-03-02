7ee6dde579b98c18df9a74d0200e58a72301f176
john <johndavidtettis@gmail.com>
2021-03-02 10:03:28 -0500

    added some responsiveness

--- app.py
+++ app.py
@@ -13,18 +13,23 @@ toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def home():
+    """Renders the home page template"""
     return render_template('home.html')
+
 @app.route('/play')
 def display_game_board():
+    """Creates game board, passes it into the session, retrieves games played, and renders the html with a leaderboard and a game board"""
     board = boggle_game.make_board()
     session['board']=board
     scores = session.get('games',[])
+    plays = len(scores)
     scores = score_truncate(scores,5)
     print('board', file=sys.stdout)
-    return render_template("boggle.html", board=board, scores=scores)
+    return render_template("boggle.html", board=board, scores=scores,nplays=plays)
 
 @app.route('/guess',methods=['POST'])
 def register_guess():
+    """handles a guess from axios request, returns whether or not the word is valid - ok, not-a-word, not-on-board, empty"""
     guess = request.json.get('guess')
     if not guess: return jsonify({'result':'empty'})
 
@@ -33,6 +38,7 @@ def register_guess():
 
 @app.route('/game/data', methods=['POST'])
 def score_keeper():
+    """Handles end-of-game score submission """
     score = request.json['score']
     games = session.get('games',default = [])
     games.append(score)
@@ -41,6 +47,7 @@ def score_keeper():
     return jsonify(games)
 
 def score_truncate(scores,length):
+    """takes an array and a length, sorts the array highest to lowest, then truncates it to length"""
     scores.sort(reverse=True)
     if len(scores)>length:
         del scores[length:]
